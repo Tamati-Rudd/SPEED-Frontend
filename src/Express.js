8da6374c442e1b0b7eed4e4cd98d856c3d772e13
vxz7784 <vxz7784@autuni.ac.nz>
2022-09-23 14:23:39 +1200

    Setup Article Submission Backend Call
    Setup call to the backend to submit an article.

---
+++ src/Express.js
@@ -0,0 +1,15 @@
+import axios from "axios";
+
+export async function submitArticle(articleData) {
+    let data = {"title":articleData}
+    try {
+        let response = await axios.post("http://localhost:4000/submit/save", data, { timeout: 10000 });
+        if (response.status === 201) {
+            return 0;
+        } 
+    } catch (error) { //500 or other error
+        console.error(error);
+        console.error("Network or server error");
+        return 1;
+    }
+}
